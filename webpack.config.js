4dc5d039ac210d5455e45c722649587a0147b800
voldmaire <levenvov@gmail.com>
2019-06-11 09:29:18 +0300

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./src/helloworld.ts",
+    output: {
+        filename: "./dist/bundle.js",
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' as resolvable extensions.
+        extensions: [".ts", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.ts$/,
+                loader: "ts-loader"
+            },
+            {
+                include: path.resolve(__dirname, "node_modules/pixi.js"),
+                enforce: "post",
+                loader: "transform?brfs"
+            }
+        ]
+    },
+    
+    externals: [
+        // Don't bundle pixi.js, assume it'll be included in the HTML via a script
+        // tag, and made available in the global variable PIXI.
+        {"pixi.js": "PIXI"}
+    ]
+
+};
