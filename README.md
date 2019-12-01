150610f422b6e4cfbb330af9420506e89bc702b6
Voldmaire <levenvov@gmail.com>
2019-12-02 00:50:56 +0200

    Add dev and prod builds, add short instructins

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # game
+
+To build project, use next command:
+```
+npm run build
+```
+
+To build in debug mode and run test-server, use command:
+```
+npm run start
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,46 @@
 var path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: "./src/Application.ts",
-    output: {
-        filename: "./bundle.js",
-    },
+module.exports = env => {
+    return {
+        entry: "./src/Application.ts",
+        output: {
+            filename: "./bundle.js",
+        },
 
-    mode: "development",
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "inline-source-map",
+        plugins: [
+            new CleanWebpackPlugin(),
+        ],
 
-    resolve: {
-        // Add '.ts' as resolvable extensions.
-        extensions: [".tsx", ".ts", ".js"]
-    },
+        mode: env === 'dev' ? "development" : "production",
+        devtool: "inline-source-map",
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: ['source-map-loader'],
-                enforce: 'pre'
-            },
-            {
-                test: /\.ts$/,
-                loader: "ts-loader"
-            },
-            {
-                include: path.resolve(__dirname, "node_modules/pixi.js"),
-                enforce: "post",
-                loader: "transform?brfs"
-            }
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: ['source-map-loader'],
+                    enforce: 'pre'
+                },
+                {
+                    test: /\.ts$/,
+                    loader: "ts-loader"
+                },
+                {
+                    include: path.resolve(__dirname, "node_modules/pixi.js"),
+                    enforce: "post",
+                    loader: "transform?brfs"
+                }
+            ]
+        },
+        externals: [
+            // Don't bundle pixi.js, assume it'll be included in the HTML via a script
+            // tag, and made available in the global variable PIXI.
+            { "pixi.js": "PIXI" }
         ]
-    },
-    externals: [
-        // Don't bundle pixi.js, assume it'll be included in the HTML via a script
-        // tag, and made available in the global variable PIXI.
-        {"pixi.js": "PIXI"}
-    ]
-
+    }
 };
