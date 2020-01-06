106b2110fe44b413c95eae69f27ec21971f9a112
Voldmaire <levenvov@gmail.com>
2020-01-06 17:07:04 +0200

    Add dto

---
+++ src/model/CellDto.ts
@@ -0,0 +1,6 @@
+import { Element } from "./Element"
+
+export class CellDto {
+    cellId: number;
+    elementI: Element;
+}
---
+++ src/model/Element.ts
@@ -0,0 +1,7 @@
+export enum Element {
+    EMPTY = 0,
+    FIRE = 1,
+    WATER = 2,
+    AIR = 3,
+    GROUND = 4
+}
