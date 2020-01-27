7ad302b8e00077d16db5d365ca36ac62278b8694
Voldmaire <levenvov@gmail.com>
2020-01-28 00:54:37 +0200

    Add TexturePacker file for image resources

--- images/convert_res90.sh
+++ images/convert_res90.sh
@@ -1,7 +1,16 @@
 readonly result_folder_name=res_90x90;
 rm -rf $result_folder_name
 mkdir $result_folder_name
-for filename in /home/voldmaire/projects/game/images/*.png; do
+mkdir $result_folder_name/basic
+cp *.tps $result_folder_name
 
-        convert $filename -resize 90x90 $result_folder_name/$(basename $filename) ;
+for filename in /home/voldmaire/projects/game/images/basic/*.png; do
+
+        convert $filename -resize 90x90 $result_folder_name/basic/$(basename $filename) ;
     done
+
+cd $result_folder_name
+for i in *.tps; do TexturePacker $i; done
+rm -f *.tps
+rm -rf basic
+cd ..
